c6d0e6150dec9f31850a78d6e7a9ed0eff37fd27
codepreneur <contramundee@gmail.com>
2014-11-13 17:21:48 +0000

    deleting friends

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(){
 		return this.store.find('friend');
+	},
+
+	actions: {
+		delete: function(friend){
+			friend.destroyRecord();
+			return false;
+		}
 	}
 });
