8474c951ff512532cbb1cea9b8d2aea6ced71ad2
codepreneur <contramundee@gmail.com>
2014-11-13 16:21:31 +0000

    visiting friends profile

--- app/router.js
+++ app/router.js
@@ -8,7 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('friends', function() { 
   	this.route('new');
+  	this.route('show', {path: ':friend_id'});
   });
+  
   
 });
 
