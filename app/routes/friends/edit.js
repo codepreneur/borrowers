68dc8b6fdb72df740978de4d414f15ae9dbace69
codepreneur <contramundee@gmail.com>
2014-11-16 10:56:45 +0000

    implemented auto saving and this.store cleanup

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	deactive: function(){
+		var model = this.modelFor('friends/edit');
+
+		if(model.get('isDirty')){
+			model.rollback();
+		}
+	}
 });
--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(){
 		return this.store.createRecord('friend');
+	},
+	deactive: function(){
+		var model = this.modelFor('friends/new');
+
+		if(model.get('isNew')){
+			model.destroyRecord();
+		}
 	}
 });
